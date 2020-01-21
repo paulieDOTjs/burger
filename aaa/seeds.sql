9002a9d99b32c8c2961701445265c1f83c941864
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2020-01-21 15:59:48 -0600

    fixing for heroku

--- aaa/seeds.sql
+++
@@ -1,8 +0,0 @@
-USE burgersDB;
-
-INSERT INTO burgers (burgerName)
-VALUES ("Baconater");
-INSERT INTO burgers (burgerName)
-VALUES ("Baconater Jr");
-INSERT INTO burgers (burgerName)
-VALUES ("Baconater Deluxe Extra Mayo");
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
-const axios = require("axios").default;
+
 
 //Connection to database
 const connect = require("./config/connection");
+
 //Connection to burger models
 const db = require("./models/Burger");
 
@@ -23,13 +23,14 @@ app.use("/api/", require("./routes/api.js"));
 
 const PORT = process.env.PORT || 3000;
 
-connect
-  .authenticate()
-  .then(() => console.log("database is connected"))
-  .catch(err => console.log(err));
+var syncOptions = { force: false };
 
-db.sequelize.sync({}).then(function() {
+db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
 });
